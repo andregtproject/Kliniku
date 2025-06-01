a3bede8d33e98a4db2ddba690b8622f751c8d172
Andre Gunawan <andretriatmaja1@gmail.com>
2025-06-01 17:21:51 +0700

    Update intent from onboarding -> AuthActivity -> CompleteProfileActivity -> BottomNavbar

--- app/src/main/java/com/kliniku/official/SplashActivity.kt
+++ app/src/main/java/com/kliniku/official/SplashActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.kliniku.official.auth.AuthActivity
 import com.kliniku.official.databinding.ActivitySplashBinding
 import com.kliniku.official.onboarding.OnboardingActivity
 import pl.droidsonroids.gif.GifDrawable
@@ -28,7 +29,7 @@ class SplashActivity : AppCompatActivity() {
             val onboardingCompleted = sharedPrefs.getBoolean("onboarding_completed", false)
 
             val intent = if (onboardingCompleted) {
-                Intent(this, BottomNavbarActivity::class.java)
+                Intent(this, AuthActivity::class.java)
             } else {
                 Intent(this, OnboardingActivity::class.java)
             }
--- app/src/main/java/com/kliniku/official/auth/profile_step/CompleteProfileActivity.kt
+++ app/src/main/java/com/kliniku/official/auth/profile_step/CompleteProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.kliniku.official.auth.profile_step
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
+import com.kliniku.official.BottomNavbarActivity
 import com.kliniku.official.R
 import com.kliniku.official.databinding.ActivityCompleteProfileBinding
 import com.kliniku.official.databinding.CustomToolbarBinding
@@ -224,6 +226,10 @@ class CompleteProfileActivity : AppCompatActivity() {
         // TODO: Submit ke database atau Firebase
 
         showToast(getString(R.string.registration_successful))
+
+        // Redirect ke BottomNavbarActivity, perlu penyesuaian lagi kalo db sudah terintegrasi
+        val intent = Intent(this, BottomNavbarActivity::class.java)
+        startActivity(intent)
 
         setResult(RESULT_OK)
         finish()
